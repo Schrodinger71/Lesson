b33b8a754313602abc4aff341ef7af76079595d4
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-03-13 16:08:01 +0300

    add miniForm

---
+++ app_WindowsForm/WindowsFormsAppCalculateLab7/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsAppCalculateLab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firsTextBox = new System.Windows.Forms.TextBox();
+            this.secondTextBox = new System.Windows.Forms.TextBox();
+            this.thistTextBoxResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton_Summ = new System.Windows.Forms.RadioButton();
+            this.radioButton_Vechsty = new System.Windows.Forms.RadioButton();
+            this.radioButton_umnojity = new System.Windows.Forms.RadioButton();
+            this.radioButton_Razdelit = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Первое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Второе число";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Результат";
+            // 
+            // firsTextBox
+            // 
+            this.firsTextBox.Location = new System.Drawing.Point(151, 41);
+            this.firsTextBox.Name = "firsTextBox";
+            this.firsTextBox.Size = new System.Drawing.Size(128, 22);
+            this.firsTextBox.TabIndex = 3;
+            // 
+            // secondTextBox
+            // 
+            this.secondTextBox.Location = new System.Drawing.Point(151, 107);
+            this.secondTextBox.Name = "secondTextBox";
+            this.secondTextBox.Size = new System.Drawing.Size(128, 22);
+            this.secondTextBox.TabIndex = 4;
+            // 
+            // thistTextBoxResult
+            // 
+            this.thistTextBoxResult.Location = new System.Drawing.Point(151, 167);
+            this.thistTextBoxResult.Name = "thistTextBoxResult";
+            this.thistTextBoxResult.Size = new System.Drawing.Size(128, 22);
+            this.thistTextBoxResult.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton_Summ
+            // 
+            this.radioButton_Summ.AutoSize = true;
+            this.radioButton_Summ.Location = new System.Drawing.Point(330, 46);
+            this.radioButton_Summ.Name = "radioButton_Summ";
+            this.radioButton_Summ.Size = new System.Drawing.Size(84, 20);
+            this.radioButton_Summ.TabIndex = 8;
+            this.radioButton_Summ.TabStop = true;
+            this.radioButton_Summ.Text = "Сложить";
+            this.radioButton_Summ.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Vechsty
+            // 
+            this.radioButton_Vechsty.AutoSize = true;
+            this.radioButton_Vechsty.Location = new System.Drawing.Point(330, 109);
+            this.radioButton_Vechsty.Name = "radioButton_Vechsty";
+            this.radioButton_Vechsty.Size = new System.Drawing.Size(83, 20);
+            this.radioButton_Vechsty.TabIndex = 9;
+            this.radioButton_Vechsty.TabStop = true;
+            this.radioButton_Vechsty.Text = "Вычесть";
+            this.radioButton_Vechsty.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_umnojity
+            // 
+            this.radioButton_umnojity.AutoSize = true;
+            this.radioButton_umnojity.Location = new System.Drawing.Point(330, 169);
+            this.radioButton_umnojity.Name = "radioButton_umnojity";
+            this.radioButton_umnojity.Size = new System.Drawing.Size(93, 20);
+            this.radioButton_umnojity.TabIndex = 10;
+            this.radioButton_umnojity.TabStop = true;
+            this.radioButton_umnojity.Text = "Умножить";
+            this.radioButton_umnojity.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Razdelit
+            // 
+            this.radioButton_Razdelit.AutoSize = true;
+            this.radioButton_Razdelit.Location = new System.Drawing.Point(330, 227);
+            this.radioButton_Razdelit.Name = "radioButton_Razdelit";
+            this.radioButton_Razdelit.Size = new System.Drawing.Size(99, 20);
+            this.radioButton_Razdelit.TabIndex = 11;
+            this.radioButton_Razdelit.TabStop = true;
+            this.radioButton_Razdelit.Text = "Разделить";
+            this.radioButton_Razdelit.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(45, 227);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(239, 20);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Выводить результат полностью";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 350);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton_Razdelit);
+            this.Controls.Add(this.radioButton_umnojity);
+            this.Controls.Add(this.radioButton_Vechsty);
+            this.Controls.Add(this.radioButton_Summ);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.thistTextBoxResult);
+            this.Controls.Add(this.secondTextBox);
+            this.Controls.Add(this.firsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firsTextBox;
+        private System.Windows.Forms.TextBox secondTextBox;
+        private System.Windows.Forms.TextBox thistTextBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton_Summ;
+        private System.Windows.Forms.RadioButton radioButton_Vechsty;
+        private System.Windows.Forms.RadioButton radioButton_umnojity;
+        private System.Windows.Forms.RadioButton radioButton_Razdelit;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ app_WindowsForm/WindowsFormsAppCalculateLab7/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace WindowsFormsAppCalculateLab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Объявляем переменные для вводимых чисел и результата
+            double x, y, z = 0;
+            //Преобразуем текст из числовых полей в переменные Double
+            x = Convert.ToDouble(firsTextBox.Text);
+            y = Convert.ToDouble(secondTextBox.Text);
+            //Выполняем операцию, тип которой определяется
+            //на основе проверки значений св-ва Checked переключателей
+            if (radioButton_Summ.Checked) z = x + y;
+            if (radioButton_Vechsty.Checked) z = x - y;
+            if (radioButton_umnojity.Checked) z = x * y;
+            //В случае деления надо убедиться, что 2-е число 
+            //не равно нулю.
+            if (radioButton_Razdelit.Checked)
+                if (y != 0)
+                    z = x / y;
+                else
+                    MessageBox.Show("На нуль делить нельзя!", "Ошибка");
+            //Преобразуем значение переменной z из типа Double 
+            //в строку и выводим ее в текстовое поле результата
+            thistTextBoxResult.Text = Convert.ToString(z);
+
+            //Проверяем флажок и, если он выбран, формируем строку 
+            //в текстовом поле результата. 
+            //Иначе выводим только одно число z
+            if (checkBox1.Checked)
+            {
+                string str_x = Convert.ToString(x);
+                string str_y = Convert.ToString(y);
+                string str_z = Convert.ToString(z);
+                if (radioButton_Summ.Checked) thistTextBoxResult.Text = str_x + "+" + str_y + "=" + str_z;
+                if (radioButton_Vechsty.Checked) thistTextBoxResult.Text = str_x + "-" + str_y + "=" + str_z;
+                if (radioButton_umnojity.Checked) thistTextBoxResult.Text = str_x + "*" + str_y + "=" + str_z;
+                if (radioButton_Razdelit.Checked && y != 0) thistTextBoxResult.Text = str_x + "/" + str_y + "="
+               + str_z;
+            }
+            else
+                thistTextBoxResult.Text = Convert.ToString(z);
+
+        }
+    }
+}
