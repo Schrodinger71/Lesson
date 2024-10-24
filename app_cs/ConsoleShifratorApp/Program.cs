814fec0390b2a42aac577ad4d97d29bb934f530a
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-10-24 23:20:18 +0300

    dd

---
+++ app_cs/ConsoleShifratorApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Program;
+
+class ProgramClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("hello wordddd");
+    }
+}
+
